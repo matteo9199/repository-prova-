0224b2943a4e7af9c3c0ca627a69a34baf56324d
matteo9199 <matteo.di.gioia@edu.immaginazioneelavoro.it>
2026-03-30 17:55:50 +0200

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,20 @@
+print("Hello,World !! Benvenuti alla lezione di python")
+
+#Commento: non viene eseguito nel codice 
+
+#1 Python è un linguaggio di programmazione:
+# - orientato agli oggetti 
+# - Procedurale
+# - Funzionale 
+# - Case Sensitive 
+# - Debolmente Tipizzato 
+
+nome = input("Come ti chiami ?")
+eta = 36 
+print, nome, "questa è la tua prima lezione di python e hai" , eta, "anni"
+    
+
+nome = input("Quanti anni hai?")
+eta = 30 
+print = nome, "la mia eta"= eta
+ eta
